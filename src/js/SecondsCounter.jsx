0f448da9a9318bccc0b95f02afa432456762e750
Caarlos3 <crt.981995@gmail.com>
2025-04-23 18:16:35 +0000

    Contador final

--- src/js/SecondsCounter.jsx
+++ src/js/SecondsCounter.jsx
@@ -1,61 +1,23 @@
 import React from "react";
 
-
-const SecondsCounter = () => {
-
-
-    const SixDigit = document.querySelector('.SixDigit');
-    const FiveDigit = document.querySelector('.FiveDigit');
-    const FourDigit = document.querySelector('.FourDigit');
-    const ThreeDigit = document.querySelector('.ThreeDigit');
-    const TwoDigit = document.querySelector('.TwoDigit');
-    const OneDigit = document.querySelector('.OneDigit');
-
-    let counter = 0;
-
-const IncrementCrono = () => {
-
-        const SixDigitV = Math.floor(counter / 100000)
-        const FiveDigitV = Math.floor(counter / 10000)
-        const FourDigitV = Math.floor(counter / 1000)
-        const ThreeDigitV = Math.floot(counter / 100)
-        const TwoDigitV = Math.floor(counter / 10)
-        const OneDigitV = Math.floor(counter / 1)
-        counter++;
-
-        SixDigit.textContent = SixDigitV;
-        FiveDigit.textContent = FiveDigitV;
-        FourDigit.textContent = FourDigitV;
-        ThreeDigit.textContent = ThreeDigitV;
-        TwoDigit.textContent = TwoDigitV;
-        OneDigit.textContent = OneDigitV;
-
-
-    };
-
-
+const SecondsCounter = ({SixDigit, FiveDigit, FourDigit, ThreeDigit, TwoDigit, OneDigit }) => {
 
     return (
         <div className="container">
             <div className="counter">
-
+    
                 <i className="fa-solid fa-clock mx-3 mt-4"></i>
-                <div className="SixDigit mx-3">0</div>
-                <div className="FiveDigit mx-3">0</div>
-                <div className="FourDigit mx-3">0</div>
-                <div className="ThreeDigit mx-3">0</div>
-                <div className="TwoDigit mx-3">0</div>
-                <div className="OneDigit mx-3">0</div>
-
+                <div className="SixDigitValue mx-3">{SixDigit}</div>
+                <div className="FiveDigitValue mx-3">{FiveDigit}</div>
+                <div className="FourDigitValue mx-3">{FourDigit}</div>
+                <div className="ThreeDigitValue mx-3">{ThreeDigit}</div>
+                <div className="TwoDigitValue mx-3">{TwoDigit}</div>
+                <div className="OneDigitValue mx-3">{OneDigit}</div>
+    
             </div>
         </div>
-
-    );
+    ); 
+    
 };
-
-
-setInterval(IncrementCrono, 1000);
-
-
 
 export default SecondsCounter;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,10 +10,33 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
-import SecondsCounte from './SecondsCounter';
+import SecondsCounter from './SecondsCounter';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SecondsCounte />
-  </React.StrictMode>,
-)
+let counter = 0
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+setInterval(() => {
+
+        const SixDigitValue = Math.floor(counter / 100000) % 10;
+        const FiveDigitValue = Math.floor(counter / 10000) % 10;
+        const FourDigitValue = Math.floor(counter / 1000) % 10;
+        const ThreeDigitValue = Math.floor(counter / 100) % 10;
+        const TwoDigitValue = Math.floor(counter / 10) % 10;
+        const OneDigitValue = Math.floor(counter / 1) % 10;
+        counter++;
+
+        root.render(
+            <SecondsCounter
+              SixDigit={SixDigitValue}
+              FiveDigit={FiveDigitValue}
+              FourDigit={FourDigitValue}
+              ThreeDigit={ThreeDigitValue}
+              TwoDigit={TwoDigitValue}
+              OneDigit={OneDigitValue}
+            />
+          );
+        
+
+}, 1000);
+
